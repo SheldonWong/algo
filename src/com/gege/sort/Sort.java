54b4bd9a72c71f7f66939f8bc7fa68fe6be6c1aa
SheldonWong <wxt0628@gmail.com>
2017-12-20 20:18:04 +0800

    init2

---
+++ src/com/gege/sort/Sort.java
@@ -0,0 +1,39 @@
+package com.gege.sort;
+
+import com.gege.utils.ArrayUtils;
+
+public class Sort {
+	
+
+	public static int partition(int[] arr,int left,int right){
+		int pivot = arr[left];
+		int small = left;
+		
+		for(int i = left+1;i <= right;i++){
+			if(arr[i] < pivot){
+				small++;
+				if(small != i){
+					ArrayUtils.swap(arr,small,i);
+				}				
+			}
+		}
+		ArrayUtils.swap(arr,left,small);
+		return small;
+	}
+	
+	public static void quickSort(int[] arr,int left,int right){
+		if(left < right){
+			int middle = partition(arr,left,right);
+			quickSort(arr,left,middle-1);
+			quickSort(arr,middle+1,right);
+		}
+	}
+
+	public static void main(String[] args) {
+		//int[] a = {8,2,7,1,4,6,3,5};
+		//生成测试用例
+		int[] a = ArrayUtils.generateRandomArray(10, 10, 30);
+		quickSort(a,0,a.length-1);
+		ArrayUtils.printArray(a);
+	}
+}
---
+++ src/com/gege/utils/ArrayUtils.java
@@ -0,0 +1,40 @@
+package com.gege.utils;
+
+public class ArrayUtils {
+	//
+	public static void printArray(int[] arr){
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	//
+	public static int[] generateRandomArray(int n,int rangeL,int rangeR){
+		assert n>0 && rangeL < rangeR;
+		int[] arr = new int[n];
+		for(int i = 0;i < n;i++){
+			arr[i] = (int)(Math.random() * (rangeR - rangeL +1) + rangeL);
+		}
+		return arr;
+	}
+	
+	//
+	public static int[] generateOrderedArray(int n){
+		assert n > 0;
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n;i++){
+			arr[i] = i;
+		}
+		
+		return arr;
+	}
+	
+	//
+	public static void swap(int[] arr,int i,int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
